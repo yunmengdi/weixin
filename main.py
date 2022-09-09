2450370fd5ee181a31a5dd0b7b776cece8f1e376
yunmengdi <113116791+yunmengdi@users.noreply.github.com>
2022-09-09 13:38:58 +0800

    Update main.py

--- main.py
+++ main.py
@@ -87,19 +87,6 @@ def get_ciba():
         print(response)
  
  
-if __name__ == "__main__":
-    try:
-        with open("config.txt", encoding="utf-8") as f:
-            config = eval(f.read())
-    except FileNotFoundError:
-        print("推送消息失败，请检查config.txt文件是否与程序位于同一路径")
-        os.system("pause")
-        sys.exit(1)
-    except SyntaxError:
-        print("推送消息失败，请检查配置文件格式是否正确")
-        os.system("pause")
-        sys.exit(1)
- 
     # 获取accessToken
     accessToken = get_access_token()
     # 接收的用户
